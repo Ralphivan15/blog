dbb4e4d7cb0c585bddca904bf388f1ac3821c223
madcute03 <madcute001@gmail.com>
2026-06-25 16:01:16 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,27 @@
 <?php
-use App\Http\Controllers\NoteController;
+
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Blog;
 
 
-Route::resource('notes', NoteController::class);
+
 Route::get('/', function () {
-    return view('welcome');
+    $blogs = Blog::all();
+    return view('welcome', compact('blogs'));
 });
+
+Route::resource('blogs', BlogController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
